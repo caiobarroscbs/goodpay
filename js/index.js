bd303b32e064de53f6c2e975f8bc43403b0be17e
caiobarroscbs <caiobarros.contato@gmail.com>
2022-09-30 10:42:45 -0300

    modulo de dados JS

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/**
+ * Trouze do arquivo moduloDados.js
+ * para o index.js
+ */
+import usuarios from "./moduloDados.js";
+
+console.log(usuarios)
+/**
+ * Disponibiliza o array para ser utilizado fora do arquivo moduloDados.js
+ */
---
+++ js/moduloDados.js
@@ -0,0 +1,40 @@
+/**
+ * Dentro deste array existem usuarios com esta estrutura:
+ * - id
+ * - nome
+ * - senha
+ * - email
+ * 
+ */
+const usuarios = [
+    {
+        id: 1,
+        nomeUsuario: "jose-almir",        
+        senha:"12345",
+        email: "jose.almir@soulcodeacademy.org",
+    },
+    {
+        id: 2,
+        nomeUsuario: "renato3x",        
+        senha:"12345",
+        email: "renato.pereira@soulcodeacademy.org",
+    },
+    {
+        id: 3,
+        nomeUsuario: "caio-barros",        
+        senha:"12345",
+        email: "caio.barros@soulcodeacademy.org",
+    },
+    {
+        id: 4,
+        nomeUsuario: "michael-doug",        
+        senha:"12345",
+        email: "michael.doug@soulcodeacademy.org",
+    },
+
+];
+
+export default usuarios;
+/**
+ * Disponibiliza o array para ser utilizado fora do arquivo moduloDados.js
+ */
